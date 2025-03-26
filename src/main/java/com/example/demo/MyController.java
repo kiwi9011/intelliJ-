f1d49ec29b50f5bcff67ad609d4acbf686ced7f5
kiwi901109 <hljhs103074@gmail.com>
2025-03-26 13:57:23 +0800

    initial commit

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Qualifier("canonPrinter")
+    @Autowired
+
+    private Printer printer;
+
+    @RequestMapping("/test")
+    public String test(){
+        printer.print("Hello World");
+        return "Hello World" ;
+
+    }
+}
